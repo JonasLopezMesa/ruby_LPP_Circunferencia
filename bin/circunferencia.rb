4064d2bf6278ad0ac5a38d3b226733ed181ffc11
77jonas77 <77jonas77@gmail.com>
2012-10-12 16:01:00 +0100

    Cálculo del radio
    Añadido la función que calcula el radio de la circunferencia

--- bin/circunferencia.rb
+++ bin/circunferencia.rb
@@ -2,11 +2,16 @@
 
 # File: circunferencia.rb
 
+# File: circunferencia.rb
+
 class Circunferencia
     
-	def initialize()
+	def initialize() 
         
 	end
     
+	def rad(perimetro) 
+		@rad = perimetro / (2 * 3.14)
+	end
     
 end
