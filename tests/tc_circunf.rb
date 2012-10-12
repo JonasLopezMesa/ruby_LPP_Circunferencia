61f9d46f21299fe488cc6cc1b0fe170b7dfc56ad
77jonas77 <77jonas77@gmail.com>
2012-10-12 16:10:21 +0100

    Añadida varible de la circunferencia
    se llama circle

--- tests/tc_circunf.rb
+++ tests/tc_circunf.rb
@@ -4,6 +4,10 @@ require "circunferencia" #fichero donde está la clase circunferencia sin .rb
 require "test/unit" #librería de pruebas unitarias
 
 class TestCircunferencia < Test::Unit::TestCase
+    def setup
+		@circle = Circunferencia.new()
+	end
     
+
     
 end
