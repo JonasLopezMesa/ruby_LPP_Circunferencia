c87cd61ed30646423d55d74ff04e6fedd73511d2
77jonas77 <77jonas77@gmail.com>
2012-10-12 16:12:20 +0100

    Añadidas dos opciones
    añadido al test la posibilidad de que le pasen algo que no es un número
    y que le pasen algo que si es un número

--- tests/tc_circunf.rb
+++ tests/tc_circunf.rb
@@ -17,10 +17,18 @@ class TestCircunferencia < Test::Unit::TestCase
 	end
     
 	# Si el test no es correcto
-	def test_incorrecto
+	def test_no_correcto
 		assert_equal(10, @circle.rad(75.36) )
 	end
     
-
+	# Cuando le paso algo que no es un número
+	def test_no_numero
+		assert_raise(RuntimeError,"El perimetro tiene que ser un numero positivo") { @circle.rad('a') }
+	end
+    
+	# Cuando le paso un perímetro negativo
+	def test_numero_negativo
+		assert_raise(RuntimeError, "No puede haber un perimetro negativo") {@circle.rad(-50)}
+	end
     
 end
