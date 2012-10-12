028db0ba680f4bfae9e98c595859ab3085ef5900
77jonas77 <77jonas77@gmail.com>
2012-10-12 16:06:08 +0100

    Creada la clase
    y descripción de los requires

--- tests/tc_circunf.rb
+++ tests/tc_circunf.rb
@@ -1,1 +1,9 @@
 # File: tc_circunferencia.rb
+
+require "circunferencia" #fichero donde está la clase circunferencia sin .rb
+require "test/unit" #librería de pruebas unitarias
+
+class TestCircunferencia < Test::Unit::TestCase
+    
+    
+end
